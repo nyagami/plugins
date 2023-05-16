2440df8b809804a30a37bb7da6b403d0e3f386ab
Ibrahim Ihusan <trulyihu@gmail.com>
2023-05-16 17:09:47 +0800

    add icon and filter

--- plugins/english/nobleMTL.js
+++ plugins/english/nobleMTL.js
@@ -1,13 +1,26 @@
-const cheerio = require('cheerio');
+﻿const cheerio = require('cheerio');
 const languages = require('@libs/languages');
 const fetchApi = require('@libs/fetchApi');
 const fetchFile = require('@libs/fetchFile');
+const FilterInputs = require('@libs/filterInputs');
 
 const pluginId = 'NobleMTL';
 const baseUrl = 'https://www.noblemtl.com/';
 
 async function popularNovels(page) {
-    const link = `${baseUrl}series/?page=${page}&status=&order=popular`;
+    let link = `${baseUrl}series/?page=${page}`;
+
+    if (filters?.genres?.length) {
+    link += filters.genres.map(i => `&genre[]=${i}`).join('');
+    }
+
+    if (filters?.type?.length) {
+    link += filters.type.map(i => `&lang[]=${i}`).join('');
+    }
+
+    link += '&status=' + (filters?.status ? filters?.status : '');
+
+    link += '&order=' + (filters?.order ? filters?.order : 'popular');
 
     const result = await fetchApi(link, {}, pluginId);
     const body = await result.text();
@@ -146,6 +159,234 @@ async function fetchImage (url){
   return await fetchFile(url, {headers: headers});
 };
 
+const filters = [
+  {
+    key: 'order',
+    label: 'Sort By',
+    values: [
+      { label: 'Default', value: '' },
+
+      { label: 'A-Z', value: 'title' },
+
+      { label: 'Z-A', value: 'titlereverse' },
+
+      { label: 'Latest Update', value: 'update' },
+
+      { label: 'Latest Added', value: 'latest' },
+
+      { label: 'Popular', value: 'popular' },
+    ],
+    inputType: FilterInputs.Picker,
+  },
+  {
+    key: 'status',
+    label: 'Status',
+    values: [
+
+      { label: 'All', value: '' },
+
+      { label: 'Ongoing', value: 'ongoing' },
+
+      { label: 'Hiatus', value: 'hiatus' },
+
+      { label: 'Completed', value: 'completed' },
+
+    ],
+    inputType: FilterInputs.Picker,
+  },
+  {
+    key: 'type',
+    label: 'Type',
+    values: [
+      { label: 'Chinese novel', value: 'chinese-novel' },
+
+      { label: 'habyeol', value: 'habyeol' },
+
+      { label: 'korean novel', value: 'korean-novel' },
+
+      { label: 'Web Novel', value: 'web-novel' },
+
+      { label: '삼심', value: '%ec%82%bc%ec%8b%ac' },
+
+      { label: '호곡', value: '%ed%98%b8%ea%b3%a1' },
+    ],
+    inputType: FilterInputs.Checkbox,
+  },
+  {
+    key: 'genres',
+    label: 'Genres',
+    values: [
+      { label: 'A.I', value: 'a.i' },
+
+      { label: 'Academy', value: 'academy' },
+
+      { label: 'Action', value: 'action' },
+
+      { label: 'Adult', value: 'adult' },
+
+      { label: 'Adventure', value: 'adventure' },
+
+      { label: 'Alternative History', value: 'alternative-history' },
+
+      { label: 'Another World', value: 'another-world' },
+
+      { label: 'Apocalypse', value: 'apocalypse' },
+
+      { label: 'Bromance', value: 'bromance' },
+
+      { label: 'Comedy', value: 'comedy' },
+
+      { label: 'Dark fantasy', value: 'dark-fantasy' },
+
+      { label: 'Demons', value: 'demons' },
+
+      { label: 'Drama', value: 'drama' },
+
+      { label: 'Dystopia', value: 'dystopia' },
+
+      { label: 'Ecchi', value: 'ecchi' },
+
+      { label: 'Entertainment', value: 'entertainment' },
+
+      { label: 'Exhaustion', value: 'exhaustion' },
+
+      { label: 'Fanfiction', value: 'fanfiction' },
+
+      { label: 'fantasy', value: 'fantasy' },
+
+      { label: 'finance', value: 'finance' },
+
+      { label: 'Full color', value: 'full-color' },
+
+      { label: 'Game', value: 'game' },
+
+      { label: 'Gender Bender', value: 'gender-bender' },
+
+      { label: 'Genius', value: 'genius' },
+
+      { label: 'Harem', value: 'harem' },
+
+      { label: 'Hero', value: 'hero' },
+
+      { label: 'Historical', value: 'historical' },
+
+      { label: 'Hunter', value: 'hunter' },
+
+      { label: 'korean novel', value: 'korean-novel' },
+
+      { label: 'Light Novel', value: 'light-novel' },
+
+      { label: 'List Adventure Manga Genres', value: 'list-adventure-manga-genres' },
+
+      { label: 'Long Strip', value: 'long-strip' },
+
+      { label: 'Love comedy', value: 'love-comedy' },
+
+      { label: 'magic', value: 'magic' },
+
+      { label: 'Manhua', value: 'manhua' },
+
+      { label: 'Martial Arts', value: 'martial-arts' },
+
+      { label: 'Mature', value: 'mature' },
+
+      { label: 'Medieval', value: 'medieval' },
+
+      { label: 'Misunderstanding', value: 'misunderstanding' },
+
+      { label: 'Modern', value: 'modern' },
+
+      { label: 'modern fantasy', value: 'modern-fantasy' },
+
+      { label: 'music', value: 'music' },
+
+      { label: 'Mystery', value: 'mystery' },
+
+      { label: 'Necromancy', value: 'necromancy' },
+
+      { label: 'No Romance', value: 'no-romance' },
+
+      { label: 'NTL', value: 'ntl' },
+
+      { label: 'o', value: 'o' },
+
+      { label: 'Obsession', value: 'obsession' },
+
+      { label: 'Politics', value: 'politics' },
+
+      { label: 'Possession', value: 'possession' },
+
+      { label: 'Programming', value: 'programming' },
+
+      { label: 'Psychological', value: 'psychological' },
+
+      { label: 'Pure Love', value: 'pure-love' },
+
+      { label: 'Redemption', value: 'redemption' },
+
+      { label: 'Regression', value: 'regression' },
+
+      { label: 'Regret', value: 'regret' },
+
+      { label: 'Reincarnation', value: 'reincarnation' },
+
+      { label: 'Revenge', value: 'revenge' },
+
+      { label: 'Romance', value: 'romance' },
+
+      { label: 'Romance Fanrasy', value: 'romance-fanrasy' },
+
+      { label: 'Salvation', value: 'salvation' },
+
+      { label: 'School Life', value: 'school-life' },
+
+      { label: 'Sci-fi', value: 'sci-fi' },
+
+      { label: 'Science fiction', value: 'science-fiction' },
+
+      { label: 'Seinen', value: 'seinen' },
+
+      { label: 'Shounen', value: 'shounen' },
+
+      { label: 'Slice of Life', value: 'slice-of-life' },
+
+      { label: 'Soft yandere', value: 'soft-yandere' },
+
+      { label: 'Sports', value: 'sports' },
+
+      { label: 'Supernatural', value: 'supernatural' },
+
+      { label: 'Survival', value: 'survival' },
+
+      { label: 'system', value: 'system' },
+
+      { label: 'Time limit', value: 'time-limit' },
+
+      { label: 'Tragedy', value: 'tragedy' },
+
+      { label: 'Transmigration', value: 'transmigration' },
+
+      { label: 'TS', value: 'ts' },
+
+      { label: 'Tsundere', value: 'tsundere' },
+
+      { label: 'Unique', value: 'unique' },
+
+      { label: 'Wholesome', value: 'wholesome' },
+
+      { label: 'Wuxia', value: 'wuxia' },
+
+      { label: 'Xuanhuan', value: 'xuanhuan' },
+
+      { label: 'Yandere', value: 'yandere' },
+
+      { label: 'Yuri', value: 'yuri' },
+    ],
+    inputType: FilterInputs.Checkbox,
+  },
+];
+
+
 module.exports = {
   id: pluginId,
   name: 'Noble Machine Translations',
@@ -160,4 +401,5 @@ module.exports = {
   parseNovelAndChapters,
   parseChapter,
   searchNovels,
+  filters,
 };
