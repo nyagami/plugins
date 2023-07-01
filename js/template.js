c3ae9b0dd09724c7118eeeecf79cf073bcda3054
SkillGG <skilletssgames@gmail.com>
2023-07-01 10:10:49 +0200

    Change template.ts

--- js/template.js
+++ js/template.js
@@ -8,31 +8,41 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const cheerio = require('cheerio');
-const fetchApi = require('@libs/fetchApi');
-const fetchFile = require('@libs/fetchFile');
-// const dayjs = require('dayjs');
-// const FilterInputs = require('@libs/filterInputs');
-// const novelStatus = require('@libs/novelStatus');
-// const isUrlAbsolute = require('@libs/isAbsoluteUrl');
-// const parseDate = require('@libs/parseDate');
-function popularNovels(pageNo) {
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fetchImage = exports.searchNovels = exports.parseChapter = exports.parseNovelAndChapters = exports.popularNovels = exports.filters = exports.site = exports.version = exports.icon = exports.name = exports.id = void 0;
+const fetchFile_1 = __importDefault(require("@libs/fetchFile"));
+// import dayjs from 'dayjs';
+// import FilterInputs from '@libs/filterInputs';
+// import novelStatus from '@libs/novelStatus';
+// import isUrlAbsolute from '@libs/isAbsoluteUrl';
+// import parseDate from '@libs/parseDate';
+exports.id = ""; // string and must be unique
+exports.name = "Source name";
+exports.icon = ""; // The relative path to the icon without @icons . For example: 'src/vi/hakolightnovel/icon.png'
+exports.version = "0.0.0"; // xx.xx.xx
+exports.site = ""; // the link to the site
+exports.filters = []; // optional
+exports["protected"] = false; // true if this site protect its resources (images) and you have to define headers or smt to bypass
+const popularNovels = function (page, { filters, showLatestNovels }) {
     return __awaiter(this, void 0, void 0, function* () {
         const novels = [];
         /*
-          Do something....
-          novel = {
-            name: '',
-            url: '',      must be absolute
-            cover: '',
-          }
-          novels.push(novel);
-        */
+        Do something....
+        novel = {
+          name: '',
+          url: '',      must be absolute
+          cover: '',
+        }
+        novels.push(novel);
+      */
         return novels;
     });
-}
-;
-function parseNovelAndChapters(novelUrl) {
+};
+exports.popularNovels = popularNovels;
+const parseNovelAndChapters = function (novelUrl) {
     return __awaiter(this, void 0, void 0, function* () {
         const novel = {
             url: novelUrl,
@@ -48,61 +58,47 @@ function parseNovelAndChapters(novelUrl) {
          * novel.genres = '';   join by commas. For example: 'romcom, action, school'
          */
         /*
-          Do something....
-          chapter = {
-            name: '',
-            url: '',      must be absoulute
-            releaseTime: '',
-          }
-          novel.chapters.push(chapter);
-        */
+    Do something....
+    chapter = {
+      name: '',
+      url: '',      must be absoulute
+      releaseTime: '',
+    }
+    novel.chapters.push(chapter);
+  */
         return novel;
     });
-}
-;
-function parseChapter(chapterUrl) {
+};
+exports.parseNovelAndChapters = parseNovelAndChapters;
+const parseChapter = function (chapterUrl) {
     return __awaiter(this, void 0, void 0, function* () {
         // Do something...
-        const chapterText = '';
+        const chapterText = "";
         return chapterText;
     });
-}
-;
-function searchNovels(searchTerm, pageNo) {
+};
+exports.parseChapter = parseChapter;
+const searchNovels = function (searchTerm) {
     return __awaiter(this, void 0, void 0, function* () {
         const novels = [];
         /*
-          Do something....
-          novel = {
-            name: '',
-            url: '',      must be absolute
-            cover: '',
-          }
-          novels.push(novel);
-        */
+        Do something....
+        novel = {
+          name: '',
+          url: '',      must be absolute
+          cover: '',
+        }
+        novels.push(novel);
+      */
         return novels;
     });
-}
-;
-function fetchImage(url) {
+};
+exports.searchNovels = searchNovels;
+const fetchImage = function (url) {
     return __awaiter(this, void 0, void 0, function* () {
         // Some site cant fetch images normally (maybe need some headers)
         // Must return base64 of image
-        return yield fetchFile(url, {});
+        return yield (0, fetchFile_1.default)(url, {});
     });
-}
-;
-module.exports = {
-    id,
-    name: 'Source name',
-    icon,
-    version,
-    site,
-    protected: false,
-    filters,
-    fetchImage,
-    popularNovels,
-    parseNovelAndChapters,
-    parseChapter,
-    searchNovels,
 };
+exports.fetchImage = fetchImage;
